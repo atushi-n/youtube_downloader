28711c10f94f74bbabe8e67fba50f3b14c11c59c
atushi <nakanoatushi413@gmail.com>
2021-08-09 21:07:14 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import youtube_dl
+import tkinter
+import threading
+
+
+def dl():
+    ydl_opts = {"outtmpl": txt_2.get() + ".mp4"}  # 辞書形式のオプション
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:  # with文で手間なく開く
+        ydl.download([txt_1.get()])  # ダウンロード
+def btn_click():
+    threading.Thread(target=dl()).start()
+
+
+
+
+root = tkinter.Tk()
+root.title("youtube_downloader")
+root.geometry("300x300")
+
+label1 = tkinter.Label(root, text='URL')
+label1.place(relx=0.1, rely=0.1)
+label2 = tkinter.Label(root, text='ファイル名')
+label2.place(relx=0.1, rely=0.3)
+
+txt_1 = tkinter.Entry(root, width=20)
+txt_1.place(relx=0.3, rely=0.1)
+txt_2 = tkinter.Entry(root, width=20)
+txt_2.place(relx=0.3, rely=0.3)
+
+btn = tkinter.Button(root, text='ダウンロード', command=btn_click)
+btn.place(relx=0.5, rely=0.5)
+
+root.mainloop()
